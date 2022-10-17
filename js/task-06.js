7cb4b0fd6cff590d4105809f9a11a2ba07ac894d
AndreyLytvynov <mrjastonehome@gmail.com>
2022-10-17 21:02:08 +0300

    Внесены правки в задание 6
    Исправлено условие проверки ввода

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,8 @@
 const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", onInpuChange);
-
 function onInpuChange(e) {
   inputEl.removeAttribute("class");
-  if (e.currentTarget.value.length >= 6) {
+  if (e.currentTarget.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.add("valid");
   } else {
     inputEl.classList.add("invalid");
